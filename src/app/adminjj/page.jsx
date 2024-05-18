6d20468947c68ba1bbc727f7533a0dada92921ee
hishamak18 <hishamak2004@gmail.com>
2024-05-18 23:28:16 +0530

    transito

---
+++ src/app/adminjj/page.jsx
@@ -0,0 +1,14 @@
+// /pages/admin/AdminPage.js
+
+import React from 'react';
+
+const AdminPage = () => {
+    return (
+        <div>
+            <h1>Admin Page</h1>
+            {/* Add your admin functionalities here */}
+        </div>
+    );
+};
+
+export default AdminPage;
