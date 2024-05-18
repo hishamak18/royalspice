ac54655bbac4589dcf04989c3a7243a82c559737
hishamak18 <hishamak2004@gmail.com>
2024-05-18 23:10:43 +0530

    transito

--- src/components/TransitionProvider.jsx
+++ src/components/TransitionProvider.jsx
@@ -16,12 +16,14 @@ const TransitionProvider = ({ children }) => {
             <div
                 key={pathName}
                 className="w-screen h-screen bg-gradient-to-b from-blue-100 to-red-100"
+
             >
                 <motion.div
-                    className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
+                    className="h-screen w-screen fixed bg-amber-500 rounded-b-[100px] z-40"
                     animate={{ height: "0vh" }}
                     exit={{ height: "140vh" }}
                     transition={{ duration: 0.5, ease: "easeOut" }}
+                    style={{ backgroundImage: "url('/herobg.png')" }}
                 />
                 <motion.div
                     className="fixed m-auto top-0 bottom-0 left-0 right-0 text-white text-8xl cursor-default z-50 w-fit h-fit"
@@ -33,9 +35,11 @@ const TransitionProvider = ({ children }) => {
                     {pathName.substring(1)}
                 </motion.div>
                 <motion.div
-                    className="h-screen w-screen fixed bg-black rounded-t-[100px] bottom-0 z-30"
+                    className="h-screen w-screen fixed bg-white-500 rounded-t-[100px] bottom-0 z-30"
                     initial={{ height: "140vh" }}
-                    animate={{ height: "0vh", transition: { delay: 0.5 } }}
+                    animate={{ height: "0vh", transition: { delay: 0.3 } }}
+                    style={{ backgroundImage: "url('/herobg.png')", opacity: "0" }}
+
                 />
                 <div className="h-24">
                     <NavBar />
