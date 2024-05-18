7ecbd28d472941bdf09ed25665060296e4fca3b5
hishamak18 <hishamak2004@gmail.com>
2024-05-18 11:08:00 +0530

    navbar

--- src/components/aboutsec/About.jsx
+++ src/components/aboutsec/About.jsx
@@ -7,7 +7,7 @@ import styles from './about.module.css';
 const About = () => {
     return (
 
-        <section class="bg-white dark:bg-gray-900">
+        <section class="bg-white ">
             <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
                 <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
                     <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">About Royal Spice Restaurant</h2>
