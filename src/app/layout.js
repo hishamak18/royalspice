bf88c9165b5eb3a379dda6fc59df03341897b425
hishamak18 <hishamak2004@gmail.com>
2024-05-18 22:55:58 +0530

    new transition thorugh navigations

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import NavBar from "@/components/navbar/NavBar";
 import Footer from "@/components/Footer/Footer";
 import FixedIcon from "@/components/fixedIcon/FixedIcon";
 import BottomNav from "@/components/bottomnav/BottomNav";
+import TransitionProvider from "@/components/TransitionProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,11 +17,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <NavBar />
-        <FixedIcon />
-        {children}
-        <BottomNav />
-        <Footer />
+        <TransitionProvider>{children}
+          <Footer />
+        </TransitionProvider>
+
       </body>
     </html>
   );
