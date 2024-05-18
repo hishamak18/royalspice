ea394425f7d4ed456a7b2fd3cac26cf679376e89
hishamak18 <hishamak2004@gmail.com>
2024-05-18 11:11:39 +0530

    footer combined with bottom nav

--- src/components/bottomnav/BottomNav.jsx
+++ src/components/bottomnav/BottomNav.jsx
@@ -28,6 +28,11 @@ const BottomNav = () => {
                     </Link>
                 </li>
             </ul>
+            <div className={` bg-gray-800 text-white py-4`}>
+                <div className="container mx-auto">
+                    <p className="text-center">© 2024 Royal Spice Restaurant. All rights reserved.</p>
+                </div>
+            </div>
         </nav>
     );
 };
