3793a39c11dcdc5a2021ac81fceb96ac3a330415
hishamak18 <hishamak2004@gmail.com>
2024-05-21 06:13:25 +0530

    blog data

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -10,22 +10,22 @@ const blogPosts = [
         id: 1,
         title: 'Delicious Recipes for the Summer',
         description: 'Discover our selection of refreshing and light recipes perfect for the hot summer months.',
-        imageSrc: '/images/blog1.jpg',
-        alt: 'Summer recipes',
+        imageSrc: 'https://images.pexels.com/photos/5410418/pexels-photo-5410418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        alt: 'Summer recipes'
     },
     {
         id: 2,
-        title: 'The Best Italian Dishes',
-        description: 'Explore the richness and variety of Italian cuisine with these must-try dishes.',
-        imageSrc: '/images/blog2.jpg',
-        alt: 'Italian dishes',
+        title: 'The Best  Dishes',
+        description: 'Explore the richness and variety of  cuisine with these must-try dishes.',
+        imageSrc: 'https://images.pexels.com/photos/20175600/pexels-photo-20175600/free-photo-of-hand-holding-coconut-shell-over-road.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        alt: 'Italian dishes'
     },
     {
         id: 3,
         title: 'Healthy Eating Tips',
         description: 'Learn how to make healthy eating a habit with our expert tips and delicious recipes.',
-        imageSrc: '/images/blog3.jpg',
-        alt: 'Healthy eating',
+        imageSrc: 'https://images.pexels.com/photos/23913881/pexels-photo-23913881/free-photo-of-typical-kerala-breakfast-porotta-and-chicken.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        alt: 'Healthy eating'
     },
 ];
 
