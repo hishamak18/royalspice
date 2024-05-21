b776d60dc27e4e23675e21b1f4390d638b2c205e
hishamak18 <hishamak2004@gmail.com>
2024-05-21 06:20:36 +0530

    fixed icon linkhref

--- src/components/fixedIcon/FixedIcon.jsx
+++ src/components/fixedIcon/FixedIcon.jsx
@@ -8,16 +8,16 @@ const FixedIcon = () => {
     return (
         <div className={`${styles.fxbtn} fixed bottom-14 right-0  z-50 flex flex-col gap-3 items-center`} >
 
-            <Link href="tel:+1234567890" className={`${styles.fxbtn} bg-green-500 text-white rounded-full p-4 shadow-lg z-50 flex items-center`}>
+            <Link href="tel:+918086624365" className={`${styles.fxbtn} bg-green-500 text-white rounded-full p-4 shadow-lg z-50 flex items-center`}>
                 <FontAwesomeIcon icon={faPhone} className={`${styles.icon} mr-2`} fill={true} />
             </Link >
-            <Link href="tel:+1234567890" className={`${styles.fxbtn}  bg-green-500 text-white rounded-full p-4 shadow-lg z-50 flex items-center`}>
+            <Link href="https://www.instagram.com/royalspiceresturant?igsh=dnBtd2gwajhzcmgx" className={`${styles.fxbtn}  bg-green-500 text-white rounded-full p-4 shadow-lg z-50 flex items-center`}>
                 <FontAwesomeIcon icon={faInstagram} className={`${styles.icon} mr-2`} fill={true} />
             </Link >
-            <Link href="tel:+1234567890" className={`${styles.fxbtn}  bg-green-500 text-white rounded-full p-4 shadow-lg z-50 flex items-center`}>
+            <Link href="https://www.facebook.com/" className={`${styles.fxbtn}  bg-green-500 text-white rounded-full p-4 shadow-lg z-50 flex items-center`}>
                 <FontAwesomeIcon icon={faFacebook} className={`${styles.icon} mr-2`} fill={true} />
             </Link >
-            <Link href="tel:+1234567890" className={`${styles.fxbtn}  bg-green-500 text-white rounded-full p-4 shadow-lg z-50 flex items-center`}>
+            <Link href="https://wa.me/8086624365" className={`${styles.fxbtn}  bg-green-500 text-white rounded-full p-4 shadow-lg z-50 flex items-center`}>
                 <FontAwesomeIcon icon={faWhatsapp} className={`${styles.icon} mr-2`} width={50} fill={true} />
             </Link >
         </div >
