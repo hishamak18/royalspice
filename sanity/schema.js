a71fb94c2b3ed89a735c1af9e883d3ddf3f05271
hishamak18 <hishamak2004@gmail.com>
2024-05-31 12:25:17 +0530

    food galleryt

--- sanity/schema.js
+++ sanity/schema.js
@@ -3,7 +3,8 @@ import { category } from './schemaTypes/category'
 import { post } from './schemaTypes/post'
 import { author } from './schemaTypes/author'
 import { menuImages } from './schemaTypes/menu'
+import { gallery } from './schemaTypes/gallery'
 
 export const schema = {
-  types: [post, author, category, menuImages, blockContent],
+  types: [post, author, category, menuImages, blockContent, gallery],
 }
---
+++ sanity/schemaTypes/gallery.js
@@ -0,0 +1,60 @@
+export const gallery = {
+    name: 'gallery',
+    title: 'Gallery',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+        },
+        {
+            name: 'slug',
+            title: 'Slug',
+            type: 'slug',
+            options: {
+                source: 'title',
+                maxLength: 96,
+            },
+        },
+        {
+            name: 'images',
+            title: 'Images',
+            type: 'array',
+            of: [
+                {
+                    type: 'image',
+                    options: {
+                        hotspot: true,
+                    },
+                    fields: [
+                        {
+                            name: 'alt',
+                            type: 'string',
+                            title: 'Alternative Text',
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'array',
+            of: [
+                {
+                    title: 'Block',
+                    type: 'block',
+                    styles: [{ title: 'Normal', value: 'normal' }],
+                    lists: [],
+                },
+            ],
+        },
+    ],
+    preview: {
+        select: {
+            title: 'title',
+            media: 'images.0',
+        },
+    },
+}
